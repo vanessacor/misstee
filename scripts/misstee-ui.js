59467e696e828e29cad1916600bac14638b558e8
vanessa <vanessapcbarros@gmail.com>
2022-12-06 16:57:13 +0100

    feat: add best score and save it localStorage

--- scripts/misstee-ui.js
+++ scripts/misstee-ui.js
@@ -1,59 +1,77 @@
-'use strict';
+"use strict";
 
 class MissteeUI {
-  constructor () {
-    this.splash = document.querySelector('.splash');
-    this.gameScreen = document.querySelector('.game');
-    this.playButton = document.getElementById('play-button');
-    this.gameOver = document.querySelector('.game-over');
-    this.finalScoreDisplay = document.getElementById('final-score');
-    this.gameWrapper = document.getElementById('canvas-wrapper');
-    this.soundOn = document.getElementById('sound');
-    this.restartButton = document.getElementById('restart-button');
-    this.sound = 'off';
+  constructor() {
+    this.splash = document.querySelector(".splash");
+    this.gameScreen = document.querySelector(".game");
+    this.playButton = document.getElementById("play-button");
+    this.gameOver = document.querySelector(".game-over");
+    this.finalScoreDisplay = document.getElementById("final-score");
+    this.gameWrapper = document.getElementById("canvas-wrapper");
+    this.soundOn = document.getElementById("sound");
+    this.restartButton = document.getElementById("restart-button");
+    this.finalScoreDisplay = document.getElementById("final-score");
+    this.bestScoreDisplay = document.getElementById("best-score");
+
+    this.scores = this.getBestScore();
+    this.sound = "off";
     this.orientationEventListener = null;
     this._bindEventListeners();
   }
 
-  _bindEventListeners () {
-    this.soundOn.addEventListener('click', () => this.turnMusicOnAndOff());
-    this.playButton.addEventListener('click', () => this.startGame());
-    this.restartButton.addEventListener('click', () => this.restartGame());
+  _bindEventListeners() {
+    this.soundOn.addEventListener("click", () => this.turnMusicOnAndOff());
+    this.playButton.addEventListener("click", () => this.startGame());
+    this.restartButton.addEventListener("click", () => this.restartGame());
   }
 
-  turnMusicOnAndOff () {
-    if (this.sound === 'off') {
-      this.sound = 'on';
-      this.soundOn.innerHTML = 'Sound <br>On';
-      this.soundOn.classList.add('sound-on');
+  getBestScore() {
+    const score =
+      localStorage.getItem("bestScore") ?? localStorage.setItem("bestScore", 0);
+    this.bestScoreDisplay.innerHTML = score;
+    return score;
+  }
+
+  updateBestScore(score) {
+    if (this.getBestScore() < score) {
+      this.scores = score;
+      localStorage.setItem("bestScore", score);
+    } else return;
+  }
+
+  turnMusicOnAndOff() {
+    if (this.sound === "off") {
+      this.sound = "on";
+      this.soundOn.innerHTML = "Sound <br>On";
+      this.soundOn.classList.add("sound-on");
     } else {
-      this.sound = 'off';
-      this.soundOn.innerHTML = 'Sound <br>Off';
-      this.soundOn.classList.remove('sound-on');
+      this.sound = "off";
+      this.soundOn.innerHTML = "Sound <br>Off";
+      this.soundOn.classList.remove("sound-on");
     }
   }
 
-  checkOrientation () {
+  checkOrientation() {
     clearTimeout(this.resumeGameDelayTimeoutId);
     this.resumeGameDelayTimeoutId = setTimeout(() => {
-      const rotatemsg = document.getElementById('rotate-screen');
+      const rotatemsg = document.getElementById("rotate-screen");
       if (window.innerWidth < window.innerHeight) {
-        rotatemsg.style.display = 'block';
+        rotatemsg.style.display = "block";
         this.game.pause();
       } else {
         const width = this.gameWrapper.clientWidth;
         const height = this.gameWrapper.clientHeight;
         this.game.updateSize(width, height);
-        rotatemsg.style.display = 'none';
+        rotatemsg.style.display = "none";
         this.game.resume();
       }
     }, 0);
   }
 
-  startGame () {
-    this.splash.style.display = 'none';
-    this.gameScreen.style.display = 'grid';
-    this.game = new Game('paused', this.sound);
+  startGame() {
+    this.splash.style.display = "none";
+    this.gameScreen.style.display = "grid";
+    this.game = new Game("paused", this.sound);
     this.game.gameOverCallBack(() => this.showGameOver(this.game.score));
     this.checkOrientation();
     this.game.start();
@@ -62,30 +80,31 @@ class MissteeUI {
       this.checkOrientation();
     };
 
-    window.addEventListener('orientationchange', this.orientationEventListener);
-    window.addEventListener('resize', this.orientationEventListener);
+    window.addEventListener("orientationchange", this.orientationEventListener);
+    window.addEventListener("resize", this.orientationEventListener);
   }
 
-  showGameOver (score) {
-    removeEventListener('orientationchange', this.orientationEventListener);
-    this.gameScreen.style.display = 'none';
-    this.gameOver.style.display = 'block';
-    this.finalScoreDisplay.innerText = 'Your score: ' + score;
+  showGameOver(score) {
+    removeEventListener("orientationchange", this.orientationEventListener);
+    this.gameScreen.style.display = "none";
+    this.gameOver.style.display = "block";
+    this.updateBestScore(score);
+    this.finalScoreDisplay.innerText = score;
   }
 
-  createLive () {
-    const lives = document.getElementById('lives');
-    const live = document.createElement('li');
+  createLive() {
+    const lives = document.getElementById("lives");
+    const live = document.createElement("li");
     lives.appendChild(live);
-    const heart = document.createElement('i');
-    heart.setAttribute('class', 'fas fa-heart');
+    const heart = document.createElement("i");
+    heart.setAttribute("class", "fas fa-heart");
     live.appendChild(heart);
   }
 
-  restartGame () {
-    const score = document.getElementById('score');
-    score.innerText = 'Score: ';
-    this.gameOver.style.display = 'none';
+  restartGame() {
+    const score = document.getElementById("score");
+    score.innerText = "Score: ";
+    this.gameOver.style.display = "none";
 
     for (let i = 0; i < 3; i++) {
       this.createLive();
